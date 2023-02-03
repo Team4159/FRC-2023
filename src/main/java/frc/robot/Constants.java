b5f04c32645090807a564d26af31715d2a628eff
Aaron Chen <aaron7431g@gmail.com>
2023-02-02 17:59:53 -0800

    feedforward constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -75,9 +75,9 @@ public final class Constants {
 
         /* Drive Motor Characterization Values 
          * Divide SYSID values by 12 to convert from volts to percent output for CTRE */
-        public static final double driveKS = (0.32 / 12); //TODO: This must be tuned to specific robot
-        public static final double driveKV = (1.51 / 12);
-        public static final double driveKA = (0.27 / 12);
+        public static final double driveKS = (0.2424 / 12); //TODO: This must be tuned to specific robot
+        public static final double driveKV = (0.7146 / 12);
+        public static final double driveKA = (0.0773 / 12);
 
         /* Swerve Profiling Values */
         /** Meters per Second */
