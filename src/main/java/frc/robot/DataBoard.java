d7b99f9645b81fbe5302a11cfea57a2cd59b16c8
Emmabot <maliu2@s.sfusd.edu>
2023-02-03 17:43:27 -0800

    shuffleboard additions (swerve angles and time)

--- src/main/java/frc/robot/DataBoard.java
+++ src/main/java/frc/robot/DataBoard.java
@@ -1,6 +1,8 @@
 package frc.robot;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -32,5 +34,14 @@ public class DataBoard extends SubsystemBase {
     @Override
     public void periodic() {
         field.setRobotPose(RobotContainer.s_Swerve.getPose()); // updates the robot's estimated position on the field
+        SwerveModulePosition[] positions = RobotContainer.s_Swerve.getPositions();
+        for(int i = 0; i < 4; i++) {
+            SmartDashboard.putNumber("swerveMod" + i, positions[i].angle.getDegrees());
+        }
+
+
+        SmartDashboard.putNumber("Game Timer", DriverStation.getMatchTime());
+        // SmartDashboard.putNumber("swerveMod0", positions[0].angle.getDegrees());
+
     }
 }
