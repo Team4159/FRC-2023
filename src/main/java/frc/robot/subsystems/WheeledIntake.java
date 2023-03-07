b71a88c2feea91ae3a7e597acbb136df378191de
Cole Lewis <colestephenlewis@gmail.com>
2023-03-06 21:54:07 -0800

    wheeled intake config neo 550s on robot init

--- src/main/java/frc/robot/subsystems/WheeledIntake.java
+++ src/main/java/frc/robot/subsystems/WheeledIntake.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import frc.robot.Constants.WheeledIntakeConstants;
 import frc.robot.Constants.WheeledIntakeConstants.WheeledIntakeState;
@@ -13,10 +14,25 @@ public class WheeledIntake {
     public WheeledIntake() {
         m1 = new CANSparkMax(WheeledIntakeConstants.wheeledIntake1Id, MotorType.kBrushless);
         m2 = new CANSparkMax(WheeledIntakeConstants.wheeledIntake2Id, MotorType.kBrushless);
+        
+        configMotors();
+
+        wheeledIntakeState = WheeledIntakeState.NEUTRAL;
+    }
+
+    public void configMotors() {
+        m1.restoreFactoryDefaults();
+        m1.setSmartCurrentLimit(20, 10);
+        m1.setIdleMode(IdleMode.kCoast);
         m1.setInverted(false);
+        m1.burnFlash();
+
+        m2.restoreFactoryDefaults();
+        m2.setSmartCurrentLimit(20, 10);
+        m1.setIdleMode(IdleMode.kCoast);
         m2.setInverted(true);
         m2.follow(m1);
-        wheeledIntakeState = WheeledIntakeState.NEUTRAL;
+        m2.burnFlash();
     }
 
     public void periodic(){
